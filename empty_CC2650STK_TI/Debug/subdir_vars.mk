e1e3f2f8e338cafef6af7ef842d283caa719d0ab
juhokais <roadtonowhere2010@protonmail.com>
2024-10-13 15:55:09 +0300

    Add files via upload

---
+++ empty_CC2650STK_TI/Debug/subdir_vars.mk
@@ -0,0 +1,74 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CFG_SRCS += \
+../empty.cfg 
+
+CMD_SRCS += \
+../CC2650STK.cmd 
+
+C_SRCS += \
+../CC2650STK.c \
+../ccfg.c \
+../helloWorld.c \
+../task_example.c \
+../uart_task.c 
+
+GEN_CMDS += \
+./configPkg/linker.cmd 
+
+GEN_FILES += \
+./configPkg/linker.cmd \
+./configPkg/compiler.opt 
+
+GEN_MISC_DIRS += \
+./configPkg/ 
+
+C_DEPS += \
+./CC2650STK.d \
+./ccfg.d \
+./helloWorld.d \
+./task_example.d \
+./uart_task.d 
+
+GEN_OPTS += \
+./configPkg/compiler.opt 
+
+OBJS += \
+./CC2650STK.obj \
+./ccfg.obj \
+./helloWorld.obj \
+./task_example.obj \
+./uart_task.obj 
+
+GEN_MISC_DIRS__QUOTED += \
+"configPkg/" 
+
+OBJS__QUOTED += \
+"CC2650STK.obj" \
+"ccfg.obj" \
+"helloWorld.obj" \
+"task_example.obj" \
+"uart_task.obj" 
+
+C_DEPS__QUOTED += \
+"CC2650STK.d" \
+"ccfg.d" \
+"helloWorld.d" \
+"task_example.d" \
+"uart_task.d" 
+
+GEN_FILES__QUOTED += \
+"configPkg/linker.cmd" \
+"configPkg/compiler.opt" 
+
+C_SRCS__QUOTED += \
+"../CC2650STK.c" \
+"../ccfg.c" \
+"../helloWorld.c" \
+"../task_example.c" \
+"../uart_task.c" 
+
+
